3996a377fdc279a24bf3e6a37022bb1c0926b230
Jonathan McPherson <jonathan@rstudio.com>
2016-04-17 21:00:07 -0700

    add background rotator to home page; improve layout on mobile

--- content/pool.md
+++ content/pool.md
@@ -3,7 +3,7 @@ date = "2016-03-26T20:49:18-07:00"
 draft = false
 title = "Pool"
 weight = 10
-banner = "img/botanic6-backdrop.jpg"
+banner = "img/botanic7-backdrop.jpg"
 menu = "main"
 +++
 
---
+++ themes/sunblind/static/js/sunblind.js
@@ -0,0 +1,27 @@
+(function() {
+
+document.addEventListener("DOMContentLoaded", function(evt) {
+  // find the background element
+  var bg = document.getElementById("background");
+
+  // skip if no element
+  if (bg === null)
+    return;
+
+  // skip if no images
+  if (!header_images)
+    return;
+
+  // skip if image already selected
+  if (bg.style.backgroundImage.indexOf("url") >= 0) 
+    return;
+
+  // select random background
+  var idx = Math.floor(header_images.length * Math.random());
+
+  // set as element background
+  bg.style.backgroundImage = "linear-gradient(rgba(0,0,0,.4), rgba(0,0,0,.4)), url(" + header_images[idx] + ")";
+});
+
+})();
+
