959b985ab790e1ef9b9e21a70ec74215c3983460
Jonathan McPherson <jonathan@rstudio.com>
2017-07-09 16:01:35 -0700

    new model for pool updates

---
+++ scripts/pool-update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# form the stub
+DATE=`date +%Y-%m-%d`
+TIMESTAMP=`date +%Y-%m-%dT%H:%M:%S%z`
+UPDATEFILE=../content/pool/$DATE.md
+
+# create the content file
+echo "+++\ndate = \"$TIMESTAMP\"\n+++\n$1\n" > $UPDATEFILE
+
+# add and commit
+git add $UPDATEFILE
+git commit -m "update pool status for $DATE"
