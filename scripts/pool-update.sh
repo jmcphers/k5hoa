65980b26f5d6a58fd922e16cde80306ad5c9803b
Jonathan McPherson <jonathan@rstudio.com>
2017-07-09 16:24:11 -0700

    so it turns out date(1) on macos doesn't produce hugo dates

--- scripts/pool-update.sh
+++ scripts/pool-update.sh
@@ -3,6 +3,7 @@
 # form the stub
 DATE=`date +%Y-%m-%d`
 TIMESTAMP=`date +%Y-%m-%dT%H:%M:%S%z`
+TIMESTAMP="${TIMESTAMP:0:22}:${TIMESTAMP:22:25}"
 UPDATEFILE=../content/pool/$DATE.md
 
 # create the content file
